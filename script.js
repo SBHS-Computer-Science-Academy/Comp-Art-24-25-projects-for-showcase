bff4c9369fa233ffe20e8a1d06be228ee0533c1a
kstewart <kstewart@sbunified.org>
2025-05-14 15:25:02 -0700

    Updated names

--- script.js
+++ script.js
@@ -1,12 +1,67 @@
+
+var names = [
+    "Alex",
+    "Hassan, Steven, and Thomas",
+    "Henry, Jayden, and Nico",
+    "Itzel",
+    "Karl",
+    "Kate",
+    "Kathy",
+    "Lily",
+    "Maddie",
+    "Nash",
+    "Noah",
+    "Shae"
+];
+
+var gridWidth = 4;
+var gridHtml = "";
+var idx = 0;
+
+makeGrid();
+
 function generateCard(names, imageName, altText, url)
 {
-  return '<div class="card text-center"> <div class="card-body"> <h5 class="card-title">'
-    + names  
+    return '<div class="card text-center"> <div class="card-body"> <h5 class="card-title">'
+    + names
     + '</h5> <p class="card-text"> <a href="'
-    + url 
-    + '" target="_blank" rel="noopener noreferrer"> <img src="images/' 
-    + imageName 
+    + url
+    + '" target="_blank" rel="noopener noreferrer"> <img src="images/'
+    + imageName
     + '" alt="'
     + altText
     + '" class="screenshots"> </a> </p> </div> </div>';
+}
+
+function makeCard() {
+    if (idx >= names.length) return;
+    var name = names[idx];
+    var file = name.replaceAll(" ","").replaceAll(",","").replace("and","");
+    var card = generateCard(name, file + ".png", "platformer game", file + "/index.html");
+    gridHtml += card; //not elegant, but it works
+    
+    idx++;
+}
+
+
+function makeGrid() {
+    
+    for (var i = 0; i < names.length; i++) {
+        if (i % gridWidth == 0) {
+            gridHtml += '<div class="row">';
+        }
+        gridHtml += '<div class="col-md-3 p-3">';
+        makeCard();
+        gridHtml += '</div>';
+        if (i % gridWidth == (gridWidth - 1)) {
+            gridHtml += '</div>';
+        }
+    }
+    
+    if (names.length % gridWidth != 0) {
+        gridHtml += '</div>';
+    }
+    
+    document.getElementById("grid").innerHTML += gridHtml;
+    
 }
